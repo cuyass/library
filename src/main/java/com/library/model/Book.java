8c70f2364c1a9fbd6f31eee84f79462d86ca18b1
Israel Espin <israel.espin92@gmail.com>
2025-03-27 11:23:00 +0100

    Creating the Book class with getters and setters

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -1,5 +1,81 @@
 package com.library.model;
 
 public class Book {
+    private int id;
+    private String title;
+    private String description;
+    private String isbn;
+    private boolean isAvailable;
+    private String author;
+    private String genre;
+
+    public Book(String title, String description, String isbn, String author, String genre){
+        this.title=title;
+        this.description=description;
+        this.isbn=isbn;
+        this.isAvailable=true;
+        this.author=author;
+        this.genre=genre;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isIsAvailable() {
+        return this.isAvailable;
+    }
+
+    public boolean getIsAvailable() {
+        return this.isAvailable;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 	
 }
