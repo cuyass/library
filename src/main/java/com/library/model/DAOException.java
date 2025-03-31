225f4b382af0e68bc9a8f1b1a0bf6a0fa6134da8
SiGu <miskybox@gmail.com>
2025-03-31 12:19:34 +0200

    FEAT - Create File DAOException

---
+++ src/main/java/com/library/model/DAOException.java
@@ -0,0 +1,6 @@
+package com.library.model;
+public class DAOException extends RuntimeException {
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+        }
+    }
