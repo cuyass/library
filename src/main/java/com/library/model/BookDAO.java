fac2f2d42c81c764808bde0c6e83340afe4b1f38
SiGu <miskybox@gmail.com>
2025-03-31 09:04:04 +0200

    FIX- typo in BookDAO

--- src/main/java/com/library/model/BookDAO.java
+++ src/main/java/com/library/model/BookDAO.java
@@ -2,7 +2,7 @@ package com.library.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import jave.sql.ResultSet;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import com.library.config.ConnectionDB;
